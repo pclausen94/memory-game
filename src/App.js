e6d5aeaceb47ab2eb4bbae44ae4bee0361c67828
pclausen94 <pclausen94@gmail.com>
2021-11-19 19:50:16 +0100

    added images, made some styling and data changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 
+const cardArray = [
+  { src: "/img/bird.jpg" },
+  { src: "/img/cat.jpg" },
+  { src: "/img/dog.jpg" },
+  { src: "/img/flamingo.jpg" },
+  { src: "/img/giraffe.jpg" },
+  { src: "/img/panda.jpg" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // Shuffle cards
+  const shuffleCards = () => {
+    const shuffleCards = [...cardArray, ...cardArray]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffleCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>hello world!</p>
+        <h1>Vendespillet</h1>
+        <button onClick={shuffleCards}>Start</button>
+
+        <div className="card-grid">
+          {cards.map((card) => (
+            <div className="card" key={card.id}>
+              <div>
+                <img className="front" src={card.src} alt="card front" />
+                <img className="back" src="/img/back.jpg" alt="card back" />
+              </div>
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
